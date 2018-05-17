840df68ce65df7d97ead3bdc9bcf138beb5eef20
Pablo <pbaeyens31@gmail.com>
2018-05-17 17:51:27 +0200

    Simplifica lista de funciones

--- funciones_objetivo.py
+++ funciones_objetivo.py
@@ -5,7 +5,7 @@ def getFunciones():
     @brief Función que devuelve una lista con las 20 funciones implementadas.
     @return Devuelve un vector con las funciones.
     '''
-    return [f1, f2, f3, f4, f5 , f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17, f18, f19, f20]
+    return [lambda x: cec2014.cec14(x,i) for i in range(1,20)]
 
 def getNumFunciones():
     '''
@@ -40,71 +40,6 @@ def getInfo(i):
     '''
     @brief Función que da la información del límite inferior, superior y el mínimo valor de la función que queramos.
     @param i Función i-ésima de la que queremos obtener información
-    @retur Devuelve un diccionario con los campos inf, sup y minimo con los datos del valor inferior de cada componente, el valor superior y el valor mínimo de la función.
+    @return Devuelve un diccionario con los campos inf, sup y minimo con los datos del valor inferior de cada componente, el valor superior y el valor mínimo de la función.
     '''
     return {'inf': getLimInf(), 'sup': getLimSup(), 'minimo': getFuncMinValor(i)}
-
-################################################################################
-##                      IMPLEMENTACIÓN DE FUNCIONES CEC                       ##
-################################################################################
-
-
-def f1(x):
-    return cec2014.cec14(x,1)
-
-def f2(x):
-    return cec2014.cec14(x,2)
-
-def f3(x):
-    return cec2014.cec14(x,3)
-
-def f4(x):
-    return cec2014.cec14(x,4)
-
-def f5(x):
-    return cec2014.cec14(x,5)
-
-def f6(x):
-    return cec2014.cec14(x,6)
-
-def f7(x):
-    return cec2014.cec14(x,7)
-
-def f8(x):
-    return cec2014.cec14(x,8)
-
-def f9(x):
-    return cec2014.cec14(x,9)
-
-def f10(x):
-    return cec2014.cec14(x,10)
-
-def f11(x):
-    return cec2014.cec14(x,11)
-
-def f12(x):
-    return cec2014.cec14(x,12)
-
-def f13(x):
-    return cec2014.cec14(x,13)
-
-def f14(x):
-    return cec2014.cec14(x,14)
-
-def f15(x):
-    return cec2014.cec14(x,15)
-
-def f16(x):
-    return cec2014.cec14(x,16)
-
-def f17(x):
-    return cec2014.cec14(x,17)
-
-def f18(x):
-    return cec2014.cec14(x,18)
-
-def f19(x):
-    return cec2014.cec14(x,19)
-
-def f20(x):
-    return cec2014.cec14(x,20)
